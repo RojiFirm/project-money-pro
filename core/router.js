3b3da2b826761ae1eee6258f8ddbe9ead285dd9b
RojiFirm <para2k25@gmail.com>
2026-07-18 14:38:16 +0800

    fix

--- core/router.js
+++ core/router.js
@@ -1,7 +1,7 @@
 const pages = {
 
     dashboard: "pages/dashboard/dashboard.html",
-    transactions: "pages/transaction/transaction.html",
+    transactions: "pages/transactions/transactions.html",
     transfer: "pages/transfer/transfer.html",
     accounts: "pages/accounts/accounts.html",
     liabilities: "pages/liabilities/liabilities.html",
